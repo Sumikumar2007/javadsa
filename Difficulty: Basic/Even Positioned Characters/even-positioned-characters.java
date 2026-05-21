9f4b59bb2c17c7b1320e97ed59cd6a4ffc0e7fda
Sumikumar2007 <tsumitkumar419@gmail.com>
2026-05-21 22:46:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Even Positioned Characters/even-positioned-characters.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static void utility(String s) {
+        // code here
+          char[] charArray = s.toCharArray();
+        for(int i=0;i<charArray.length;i++)
+        {
+            if(i==0||i%2==0)
+            {
+                System.out.print(charArray[i]); 
+            }
+        }
+    }
+}
